51e9a2463f27c6df513134bcd4b06db645c0a4d5
Leo Moon <haha978@naver.com>
2023-11-21 23:31:39 -0800

    RND DTC n_order, num_x_pulses in left and right

--- MATLAB-master/Instr/applications/direct/TecmagFullOp_9484_DDC_pulseGenV2_RND_DTC.m
+++ MATLAB-master/Instr/applications/direct/TecmagFullOp_9484_DDC_pulseGenV2_RND_DTC.m
@@ -277,28 +277,31 @@ end
     lengths(3) = 0.97*108e-6;
     %set random seed
     seed = 5;
-%     num_x_lt_pulse = 5 + mod(index, 6);
-    
+	num_x_lt_pulses = 5 + mod(index, 6);
+    index_2 = mod(fix(index/6), 6);
+    index_3 = fix(fix(index/6)/6);
+    num_x_rt_pulses = 5 + index_2;
+    n_order = index_3;
     fprintf("This is the random seed %d \n", seed);
     fprintf("This is the length of the pi+e pulse %d \n", lengths(3));
+    fprintf("This is number of n-RMD order %d\n", n_order);
     phases = [0 90 0 90];
     mods = [0 0 0 0]; %0 = square, 1=gauss, 2=sech, 3=hermite
     % readout after all pulses
     spacings = [5e-6 25e-6 25e-6 25e-6];
-    fprintf("This is x-pulse spacings %d", spacings(4));
+    fprintf("This is x-pulse spacings %d \n", spacings(4));
     trigs = [0 1 1 1];
     markers = [1 1 1 1]; %always keep these on => turns on the amplifier for the pulse sequence
     reps = [1 6000 1 300];
     % the number of repetitions to create DTC once polarization stabilizes
-    DTC_rep_seq = 720;
-    num_x_lt_pulses = 40;
+    DTC_rep_seq = 1000;
+    reps(4) = num_x_lt_pulses + num_x_rt_pulses;
     fprintf("This is the number of x-pulses left of the Y pulse: %d \n", num_x_lt_pulses);
+    fprintf("This is the number of x-pulses right of the Y pulse: %d \n", num_x_rt_pulses);
     assert(num_x_lt_pulses < reps(4), "number of x-pulses applied, left of the Y pulse should be less than all x pulses in a block");
     % generate random seq of 2s and 3s with length RMD_seq_length.
     % it will be used to indicate which segment to use when generating
     % tasktable
-    n_order = 0;
-    fprintf("This is number of n-RMD order %d\n", n_order);
     RMD_seq_len = floor(DTC_rep_seq/(2^n_order));
     assert(DTC_rep_seq == RMD_seq_len*(2^n_order), "DTC_rep_seq should be multiple of 2^n_order");
     random_seq = get_n_random_seq(RMD_seq_len, seed, n_order);
