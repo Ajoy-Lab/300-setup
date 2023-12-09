b2215336bd904c044de67af5d72daad970e247e9
Leo Moon <haha978@naver.com>
2023-12-08 17:00:22 -0800

    FID tof1

--- MATLAB-master/Instr/applications/direct/TecmagFullOp_9484_DDC_pulseGenV2_FID.m
+++ MATLAB-master/Instr/applications/direct/TecmagFullOp_9484_DDC_pulseGenV2_FID.m
@@ -233,8 +233,7 @@ end
     
     reps = [1];
     repeatSeq = [1]; % how many times to repeat the block of pulses
-    
-                tof = -1000*(25.0613);
+                tof = -1000*(24.4983);
                 
                 ch=1;
                 initializeAWG(ch);
