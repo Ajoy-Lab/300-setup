ce923bd424bcf870e9400e5dd5c093cdf3c0e23b
Leo Moon <haha978@naver.com>
2024-01-05 16:44:25 -0800

    Thue Morse sequence updated

--- MATLAB-master/Instr/applications/direct/TecmagFullOp_9484_DDC_pulseGenV2_Thue_Morse_seq.m
+++ MATLAB-master/Instr/applications/direct/TecmagFullOp_9484_DDC_pulseGenV2_Thue_Morse_seq.m
@@ -269,13 +269,11 @@ end
     pi_half = 52.05e-6;
     pi = 104.10e-6;
     %% DEFINE PULSE SEQUENCE PARAMETERS
-    index = cmdBytes(2);
+%     index = cmdBytes(2);
     amps = [1 1 1 1];
     frequencies = [0 0 0 0];
     %[pi/2 Y-pulse, theta x-pulse(spin lock), pi Y-pulse, pi/2 x-pulse]
     lengths = [pi_half pi_half pi pi_half];
-    %set random seed
-    seed = fix(index/8) + 1; 
     fprintf("This is the length of the pi+e pulse %d \n", lengths(3));
     phases = [0 90 0 90];
     mods = [0 0 0 0]; %0 = square, 1=gauss, 2=sech, 3=hermite
