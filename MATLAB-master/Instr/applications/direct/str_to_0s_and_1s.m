9678a07fbda50edd82a843d1de6e54e735aa2f63
Leo Moon <haha978@naver.com>
2024-02-26 11:01:22 -0800

    str_to_0s_and_1s function's bug fixed

--- MATLAB-master/Instr/applications/direct/str_to_0s_and_1s.m
+++ MATLAB-master/Instr/applications/direct/str_to_0s_and_1s.m
@@ -5,9 +5,18 @@ function arr = str_to_0s_and_1s(str1)
     arr_utf_16 = uint16(str1);
     char_arr = dec2bin(arr_utf_16);
     size_char_arr = size(char_arr);
-    length_char_arr = size_char_arr(1) * size_char_arr(2);
+    str_arr = '';
+    for i = (1:size_char_arr(1))
+        str_arr = append(str_arr, char_arr(i,:));
+    end
     arr = [];
-    for i = (1:length_char_arr)
-        arr(end+1) = str2double(char_arr(i));
+    for i = (1:length(str_arr))
+        if str_arr(i) == '1'
+            arr(end+1) = 1;
+        elseif str_arr(i) == '0'
+            arr(end+1) = 0;
+        else
+            fprintf("str_arr should be only 1s and 0s")
+        end
     end
 end
