c681ffa14e42aa5c0aa0702f6387cf36403a97d9
Leo Moon <haha978@naver.com>
2024-07-05 14:52:37 -0700

    harmonics scan N=16

--- MATLAB-master/Instr/applications/direct/TecmagFullOp_9484_DDC_pulseGenV2_DTC_ACsensing.m
+++ MATLAB-master/Instr/applications/direct/TecmagFullOp_9484_DDC_pulseGenV2_DTC_ACsensing.m
@@ -248,8 +248,8 @@ end
     trigs = [0 1 1 1]; %acquire on every "pi" pulse
     
     
-    reps = [1 6000 1 4];
-    repeatSeq = [1 32000]; % how many times to repeat the block of pulses
+    reps = [1 6000 1 16];
+    repeatSeq = [1 16000]; % how many times to repeat the block of pulses
     
     fprintf("setting up pulse blaster sequence\n");
     PB = containers.Map('KeyType', 'double', 'ValueType', 'any');
@@ -293,7 +293,9 @@ end
     freq_mult_idx = mod(fix(idx/25),5) + 1;
     vpp_idx = fix(idx/(25*5)) + 1;
     freq = reso_freq*freq_multiplier(freq_mult_idx) + freq_offset(freq_offset_idx);
-    
+
+    AC_dict.freq = reso_freq;
+    AC_dict.Vpp = 1;
     AC_dict.freq = freq;
     AC_dict.Vpp = vpp_l(vpp_idx);
     AC_dict.phase = 90;
