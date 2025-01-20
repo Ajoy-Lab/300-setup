61264e10aa01446d0ef6726a96ac34ebd3d93334
Leo Moon <haha978@naver.com>
2025-01-20 04:02:20 -0800

    Rabi osci. v2

--- MATLAB-master/Instr/applications/direct/TecmagFullOp_9484_DDC_pulseGenV2.m
+++ MATLAB-master/Instr/applications/direct/TecmagFullOp_9484_DDC_pulseGenV2.m
@@ -278,7 +278,7 @@ end
     markers2 = [0 0];
     trigs = [0 1]; %acquire on every "pi" pulse
     
-    reps = [1 1000000];
+    reps = [1 200000];
     repeatSeq = [1]; % how many times to repeat the block of pulses
     
                 tof = cmdBytes(6);
